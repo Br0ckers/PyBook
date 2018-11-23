828f4706455bb50305abe6b1b9236230f88bd117
David Best <david_best@ntlworld.com>
2018-11-23 14:42:30 +0000

    updated views

--- pybookapp/msg_board/urls.py
+++ pybookapp/msg_board/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from msg_board import views
 
 
@@ -6,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^user', views.UserListView.as_view()),
     url(r'^player', views.UserListProperView.as_view()),
-
+    path('home/', views.home, name='pybook-home'),
+    path('about/', views.about, name='pybook-about'),
 ]
