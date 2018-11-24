ce067aa2536622470798f5cdfef5b833b71970dc
PreetiUdas <preetiudas@yahoo.com>
2018-11-24 21:06:59 +0000

    solved issue with create user

--- pybookapp/msg_board/models.py
+++ pybookapp/msg_board/models.py
@@ -1,6 +1,8 @@
 #from django.db import models
 from djongo import models
 from django import forms
+from django.utils import timezone
+
 
 
 # # Create your models here.
@@ -8,8 +10,9 @@ from django import forms
 
 class Message(models.Model):
     text = models.TextField(max_length=200)
-    date = models.DateField()
+    date = models.DateField(null=True, default = timezone.localtime())
     like_count = models.IntegerField(default=0)
+    #meta_data = models.EmbeddedModelField(model_container=MetaData)
     objects = models.DjongoManager()
 
     def __str__(self):
@@ -18,13 +21,22 @@ class Message(models.Model):
 
 class Player(models.Model):
     user_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
+    email = models.EmailField()
     password = models.CharField(max_length=15, default='password')
     last_login = models.DateField(null=True, default = '2018-01-01')
-    last_ipaddress = models.CharField(max_length=20, blank=True)
-    message_list = models.ArrayReferenceField(to=Message, null=True, default = '')
+    last_ipaddress = models.CharField(max_length=20, null=True)
+    message_list = models.ArrayReferenceField(to=Message, null=True, blank=True)
     #messagelist = models.ArrayReferenceField(to=Message, on_delete = models.CASCADE)
     objects = models.DjongoManager()
 
     def __str__(self):
         return self.user_name
+
+# class MetaData(models.Model):
+#     pub_date = models.DateField()
+#     mod_date = models.DateField()
+#     n_pingbacks = models.IntegerField()
+#     rating = models.IntegerField()
+#
+#     class Meta:
+#         abstract = True
--- pybookapp/msg_board/urls.py
+++ pybookapp/msg_board/urls.py
@@ -3,8 +3,18 @@ from msg_board import views
 
 
 urlpatterns = [
+
+    # old stuff
     url(r'^$', views.HomePageView.as_view()),
     url(r'^user', views.UserListView.as_view()),
     url(r'^player', views.UserListProperView.as_view()),
+
+    #new stuff
+    url(r'^user/?$', views.ViewUsers.as_view(), name='user-list'),
+    url(r'^user/update/(?P<id>\d+)/$', views.UpdateUser.as_view(), name='user-update'),
+    url(r'^user/login/(?P<id>\d+)/$', views.LoginUser.as_view(), name='user-login'),
+    url(r'^user/forgot/(?P<id>\d+)/$', views.RemindPasswordUser.as_view(), name='user-password-remind'),
+    url(r'^user/create/$', views.CreateUser.as_view(), name='user-create'),
+    url(r'^user/delete/(?P<id>\d+)/$', views.DeleteUser.as_view(), name='user-delete'),
 
 ]
