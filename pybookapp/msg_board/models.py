7954ecddcebe4917d6a09c491a6776b487df3f50
Sundar Venkataraman <phi.pax@gmail.com>
2018-11-28 17:22:13 +0000

    resolved conflicts

--- pybookapp/msg_board/models.py
+++ pybookapp/msg_board/models.py
@@ -4,22 +4,6 @@ from django.utils import timezone
 from django.conf import  settings
 from django.contrib.auth.models import User
 
-class Message(models.Model):
-    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='%(class)s_username', )
-    text = models.TextField(max_length=200)
-    date = models.DateField(null=True, default = timezone.now)
-    like_count = models.IntegerField(default=0)
-    objects = models.DjongoManager()
-
-    def __str__(self):
-        return self.text
-
-    def increment_like(self):
-        self.like_count = self.like_count + 1
-
-    def get_absolute_url(self):
-        return reverse('message_detail', kwargs={'pk': self.pk})
-
 class Member(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete = models.CASCADE)
     nickname = models.CharField(max_length=30)
@@ -29,16 +13,12 @@ class Member(models.Model):
 
     def __str__(self):
         return self.nickname
-    
-    
-
     def get_absolute_url(self):
         return reverse('member_detail', kwargs={'pk': self.pk})
 
-
 class Message(models.Model):
     text = models.TextField(max_length=200)
-    date = models.DateField(null=True, default = timezone.now())
+    date = models.DateField(null=True, default = timezone.now)
     like_count = models.IntegerField(default=0)
     created_by = models.ForeignKey(Member, on_delete=models.CASCADE)
     objects = models.DjongoManager()
