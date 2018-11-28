6aad14f55fbbcef72ad65cc1ac6194152262731a
David Best <david_best@ntlworld.com>
2018-11-28 10:26:22 +0000

    updated forms.py to import Message and Member

--- pybookapp/msg_board/forms.py
+++ pybookapp/msg_board/forms.py
@@ -1,16 +1,17 @@
 from django import forms
+from msg_board.models import Message, Member
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 
 
-class MessageForm(ModelForm):
+class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['text', 'date', 'like_count']
 
-class MemberForm(ModelForm):
+class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['name', 'email', 'password']
